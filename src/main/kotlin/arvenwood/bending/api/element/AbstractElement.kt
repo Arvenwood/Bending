b707cf135d9155418fe237cdce786947a24ad23b
xDotDash <xdotdash@gmail.com>
2020-01-28 13:37:27 -0600

    Add equals and hashcode

--- src/main/kotlin/arvenwood/bending/api/element/AbstractElement.kt
+++ src/main/kotlin/arvenwood/bending/api/element/AbstractElement.kt
@@ -3,4 +3,13 @@ package arvenwood.bending.api.element
 abstract class AbstractElement(private val id: String, private val name: String = id) : Element {
     override fun getId(): String = id
     override fun getName(): String = name
+
+    override fun equals(other: Any?): Boolean = when {
+        this === other -> true
+        other !is AbstractElement -> false
+        else -> this.id == other.id && this.name == other.name
+    }
+
+    override fun hashCode(): Int =
+        31 * id.hashCode() + name.hashCode()
 }
