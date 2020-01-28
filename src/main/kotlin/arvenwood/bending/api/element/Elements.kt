5c49be370f4421278c0423e0cc62a8d1a2d95f83
xDotDash <xdotdash@gmail.com>
2020-01-28 13:37:45 -0600

    Add Elements.Water and Elements.Earth

--- src/main/kotlin/arvenwood/bending/api/element/Elements.kt
+++ src/main/kotlin/arvenwood/bending/api/element/Elements.kt
@@ -5,11 +5,19 @@ import org.spongepowered.api.text.format.TextColors
 
 object Elements {
 
-    object Air : AbstractElement("bending:air", "Air Element") {
-        override val color: TextColor = TextColors.GRAY
+    object Water : AbstractElement("bending:water", "Water Element") {
+        override val color: TextColor = TextColors.AQUA
+    }
+
+    object Earth : AbstractElement("bending:earth", "Earth Element") {
+        override val color: TextColor = TextColors.GREEN
     }
 
     object Fire : AbstractElement("bending:fire", "Fire Element") {
         override val color: TextColor = TextColors.RED
+    }
+
+    object Air : AbstractElement("bending:air", "Air Element") {
+        override val color: TextColor = TextColors.GRAY
     }
 }
